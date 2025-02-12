b58aab6d43284fb20ce97e02fa644d623e25c228
ThranduilUrM0m <zakariaeboutaleb@gmail.com>
2025-02-12 23:30:37 +0100

    Bonjour !

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 ﻿export default function HomePage() {
   return (
     <main>
-      <h1>Welcome to the Homepage, Hello !</h1>
+      <h1>Welcome to the Homepage, Bonjour !</h1>
       <p>This is the public homepage.</p>
     </main>
   );
