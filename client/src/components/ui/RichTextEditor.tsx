fe086594f13a3ed83eadcfcc6c6c232d8846f1ee
ThranduilUrM0m <zakariaeboutaleb@gmail.com>
2025-06-18 00:03:51 +0100

    ImageResize

--- client/src/components/ui/RichTextEditor.tsx
+++ client/src/components/ui/RichTextEditor.tsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchFontList, selectFontList } from '@/slices/fontsSlice';
 import ToolbarSelect from '@/components/ui/ToolbarSelect';
 import SimpleBar from 'simplebar-react';
+import { ImageResize } from 'quill-image-resize-module';
 import 'quill/dist/quill.snow.css'; // Import Quill styles
 
 // ➊ Minimal types for our handler
@@ -32,6 +33,7 @@ FontAttributor.whitelist = [];
 SizeAttributor.whitelist = [];
 Quill.register('formats/font', FontAttributor, true);
 Quill.register('formats/size', SizeAttributor, true);
+Quill.register('modules/imageResize', ImageResize);
 
 interface RichTextEditorProps {
     value?: string;
@@ -104,6 +106,10 @@ const RichTextEditor = forwardRef<RichTextEditorHandle, RichTextEditorProps>(
                     modules: {
                         toolbar: '#toolbar',
                         history: { delay: 500, maxStack: 200, userOnly: true },
+                        imageResize: {
+                            // Optional: see quill-image-resize-module docs for config options
+                            modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
+                        }
                     },
                     formats: [
                         'font',
